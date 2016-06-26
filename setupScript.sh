0d2fed32aad6a63bae1f6ad0f7e2e5bcae5d7a27
rohanchhipa <rohan.chhipa@live.com>
2016-06-26 08:12:06 +0200

    Added maven installation

--- setupScript.sh
+++ setupScript.sh
@@ -1,5 +1,9 @@
 sudo -s
-	
+
+mkdir .tmpDir
+cd .tmpDir/
+
+#Essentials XD
 add-apt-repository -y ppa:webupd8team/java
 add-apt-repository -y ppa:webupd8team/sublime-text-3
 
@@ -9,8 +13,23 @@ apt-get dist-upgrade -y
 apt-get install oracle-java8-installer -y
 apt-get install sublime-text-installer -y
 
+apt-get install gdebi -y
+
+apt-get install kile -y
+	
+#Maven install (This link might need to be updated from time to time)
+wget http://apache.saix.net/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+tar xzvf apache-maven-3.3.9-bin.tar.gz
+mv apache-maven-3.3.9 /opt/
+ln -s /opt/apache-maven-3.3.9/bin/mvn /usr/local/bin/mvn
+chmod 755 /usr/local/bin/mvn
+
 #Improve overall system performance
 echo vm.swappiness = 10 >> /etc/sysctl.conf
 echo vm.vfs_cache_pressure = 50 >> /etc/sysctl.conf
 
+apt-get autoremove
+apt-get autoclean
+
+cd ..
 reboot
