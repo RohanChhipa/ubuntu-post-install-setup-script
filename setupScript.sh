ae3a7970115bed61eed004b60b46f6e5f8e48e64
rohanchhipa <rohan.chhipa@live.com>
2016-06-26 22:42:07 +0200

    Added yasm

--- setupScript.sh
+++ setupScript.sh
@@ -10,25 +10,27 @@ add-apt-repository -y ppa:webupd8team/sublime-text-3
 apt-get update
 apt-get dist-upgrade -y
 
+#Compilers
 apt-get install oracle-java8-installer -y
-apt-get install sublime-text-installer -y
+apt-get install gcc -y
+apt-get install yasm -y
 
 #Compression algorithms
 apt-get install rar bzip2 zip p7zip  -y
 
+#Package managers
 apt-get install gdebi -y
 
 #Tex editor along with most of the important LaTex files
 apt-get install kile -y
 
+#Text Editors
+apt-get install sublime-text-installer -y
 apt-get install scite -y
-
-apt-get install gcc -y
 	
 #Maven install (This link might need to be updated from time to time)
 wget http://apache.saix.net/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 tar -xzvf apache-maven-3.3.9-bin.tar.gz -C /opt/
-#mv apache-maven-3.3.9 /opt/
 ln -s /opt/apache-maven-3.3.9/bin/mvn /usr/local/bin/mvn
 chmod 755 /usr/local/bin/mvn
 
